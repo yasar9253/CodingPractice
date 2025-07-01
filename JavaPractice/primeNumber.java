dfa818d73a0eb7b9c7ea7ccdd2094123dd253e21
Yasar Harafath <yasar9253@gmail.com>
2025-07-01 17:47:25 +0530

    isprime function added in primeNumber.java"
    22

--- JavaPractice/primeNumber.java
+++ JavaPractice/primeNumber.java
@@ -1,7 +1,25 @@
 package JavaPractice;
 
+import java.util.Scanner;
+
 public class primeNumber {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+        isprime(num);       
+    }
+
+    public static void isprime(int a){
+        if(a == 0 || a== 1) {System.out.println("false"); return;}
+        int c = 2;
+        while(c*c<=a){
+            if(a%c == 0){
+                System.out.println("false"); return;
+            }
+            c++;
+        }
+        System.out.println("True");
+
     }
 }
